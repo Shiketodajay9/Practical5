5c2fa15197e41c9fc3d85ddddb5afd219e06d979
ajayshik <ajayshik@OZKL02.corp.amdocs.com>
2022-11-12 16:45:10 +0530

    practical5

---
+++ src/Practical5/Class.java
@@ -0,0 +1,9 @@
+package Practical5;
+import java.lang.*;
+public class Class{  
+public static void main(String args[]){  
+String s1="HeLlo";  
+String s1lower=s1.toLowerCase();  
+System.out.println(s1lower);  
+}}  
+
